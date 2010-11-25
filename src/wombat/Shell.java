43ad5778b7334c1d1fb24f8c359dd6aa74729f8e
Simon Berger <bergers@in.tum.de>
2010-11-25 11:21:32 +0100

    wrote 'fork-free' executor service

---
+++ src/wombat/Shell.java
@@ -0,0 +1,79 @@
+package wombat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+
+
+public class Shell {
+	Process pr;
+	StreamConsumerThread ec;
+	BufferedReader stdout;
+	OutputStream stdin;
+	
+	Shell() {
+		try {
+			pr = Runtime.getRuntime().exec( "sh" );
+			
+			ec = new StreamConsumerThread(pr.getErrorStream(), null );
+			
+			stdout = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			stdin = pr.getOutputStream();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException( "bailing out." );
+		}
+	}
+	
+	
+	ArrayList<String> exec( String cmd ) {
+		ArrayList<String> ret = new ArrayList<String>();
+		
+		
+		
+		try {
+			long time1 = System.currentTimeMillis();
+			
+			stdin.write(cmd.getBytes());
+			stdin.write('\n');
+			
+			stdin.write( "echo @end\n".getBytes() );
+			stdin.flush();
+			String line;
+			while( (line = stdout.readLine()) != null ) {
+				//System.out.printf( "out: '%s' %d\n", line, System.currentTimeMillis() - time1 );
+				
+				if( line.equals("@end")) {
+					break;
+				} else {
+					ret.add(line);
+				} 
+			}
+			System.out.printf( "done: %d\n", System.currentTimeMillis() - time1 );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException( "bailing out." );
+		}
+		
+		
+		
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		//int[] arr = new int[1024 * 1024 * 256];
+		
+		Shell sh = new Shell();
+		ArrayList<String> out = sh.exec( "ls ~/" );
+		
+		for( String l : out ) {
+			System.out.printf( "out: '%s'\n", l );
+		}
+	}
+}
